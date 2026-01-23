27e367bf64bf37b9cc15c2ef9d77589552212e91
solemnla <solemnla@outlook.com>
2026-01-23 21:31:56 +0800

    Uploaded environments and installation codes

---
+++ 00_environment_set_up.sh
@@ -0,0 +1,11 @@
+conda env create -f ./environments/depict.yml && \
+    conda env create -f ./environments/pops.yml && \
+    conda env create -f ./environments/ldsc.yml && \
+    conda env create -f ./environments/NetworkX.yml && \
+    conda env create -f ./environments/py37.yml && \
+    conda env create -f ./environments/py39.yml
+
+conda env create -f ./environments/r.yml && \
+    /bin/bash -c "source activate r && R -e 'devtools::install_github(\"ZikunY/CARMA\")' && \
+    R -e 'remotes::install_github(\"gabraham/plink2R\", subdir=\"plink2R\", ref=\"master\")' && \
+    R -e 'remotes::install_github(\"cotsapaslab/jlim\", subdir=\"jlimR\", ref=\"master\")'"
