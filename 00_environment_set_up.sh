205ee059a32f2f25079bcde432e22c8ba14628f8
YaJoo <guoyazhou@westlake.edu.cn>
2026-01-27 11:06:41 +0800

    GAMMA test  update

--- 00_environment_set_up.sh
+++ 00_environment_set_up.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # You should have conda installed before running this script.
 
-conda env create -f ./environments/depict.yml && \
-    conda env create -f ./environments/pops.yml && \
-    conda env create -f ./environments/ldsc.yml && \
-    conda env create -f ./environments/NetworkX.yml && \
-    conda env create -f ./environments/py39.yml
+conda env create -f ./environments/GAMMA_depict.yml && \
+    conda env create -f ./environments/GAMMA_pops.yml && \
+    conda env create -f ./environments/GAMMA_ldsc.yml && \
+    conda env create -f ./environments/GAMMA_NetworkX.yml && \
+    conda env create -f ./environments/GAMMA_ML.yml
 
-conda env create -f ./environments/r.yml && \
-    /bin/bash -c "source activate GAMMA_r && R -e 'devtools::install_github(\"ZikunY/CARMA\")' && \
-    R -e 'remotes::install_github(\"gabraham/plink2R\", subdir=\"plink2R\", ref=\"master\")' && \
-    R -e 'remotes::install_github(\"cotsapaslab/jlim\", subdir=\"jlimR\", ref=\"master\")'"
-
-# bash ./environments/gamma_r.sh && \
-#     /bin/bash -c "source activate GAMMA_r && \
-#     R -e 'install.packages(c(\"htmltools\", \"remotes\"), repos=\"https://cran.r-project.org\")' && \
-#     R -e 'devtools::install_github(\"ZikunY/CARMA\")' && \
+# conda env create -f ./environments/GAMMA_r.yml && \
+#     /bin/bash -c "source activate GAMMA_r && R -e 'devtools::install_github(\"ZikunY/CARMA\")' && \
 #     R -e 'remotes::install_github(\"gabraham/plink2R\", subdir=\"plink2R\", ref=\"master\")' && \
 #     R -e 'remotes::install_github(\"cotsapaslab/jlim\", subdir=\"jlimR\", ref=\"master\")'"
+
+bash ./environments/GAMMA_r.sh && \
+    /bin/bash -c "source activate GAMMA_r && \
+    R -e 'install.packages(c(\"htmltools\", \"remotes\"), repos=\"https://cran.r-project.org\")' && \
+    R -e 'devtools::install_github(\"ZikunY/CARMA\")' && \
+    R -e 'remotes::install_github(\"gabraham/plink2R\", subdir=\"plink2R\", ref=\"master\")' && \
+    R -e 'remotes::install_github(\"cotsapaslab/jlim\", subdir=\"jlimR\", ref=\"master\")'"
